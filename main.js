0157b4ce9d72db52de440d6ab489569a3fde2fba
JoseLuisReySaavedraADSO <reysaavedra123@gmail.com>
2022-11-01 11:31:55 -0500

    Punto 1

---
+++ main.js
@@ -0,0 +1,7 @@
+let num1 = Number(prompt(`Ingrese el numero que desee aplicar`))
+let num2 = Number(prompt(`Ingrese el numero que desee aplicar`))
+suma = num1+num2
+resta = num1-num2
+mul = num1*num2
+div = num1/num2
+console.log(`Resultados: \nResultado de la suma es: ${suma} \nResultado de la resta es ${resta} \nResultado de la multplicacion es ${mul} \nResultado de la division es ${div}`)
