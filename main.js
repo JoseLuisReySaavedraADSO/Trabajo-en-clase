8de98c40dfacb584e91fcf26e9b86f09305aa32f
JoseLuisReySaavedraADSO <reysaavedra123@gmail.com>
2022-11-01 11:39:23 -0500

    Punto 2

--- main.js
+++ main.js
@@ -1,7 +1,5 @@
-let num1 = Number(prompt(`Ingrese el numero que desee aplicar`))
-let num2 = Number(prompt(`Ingrese el numero que desee aplicar`))
-suma = num1+num2
-resta = num1-num2
+alert(`Ingrese a continuacion los lados del rectangulo`)
+let num1 = Number(prompt(`Medida del lado 1`))
+let num2 = Number(prompt(`Medida del lado 2`))
 mul = num1*num2
-div = num1/num2
-console.log(`Resultados: \nResultado de la suma es: ${suma} \nResultado de la resta es ${resta} \nResultado de la multplicacion es ${mul} \nResultado de la division es ${div}`)
+console.log(`El area del rectangulo es ${mul}`)
