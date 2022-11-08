a72076c0156c6d9c1be1ada164b4128f9462ce24
JoseLuisReySaavedraADSO <reysaavedra123@gmail.com>
2022-11-08 08:38:17 -0500

    Punto 19

--- main.js
+++ main.js
@@ -1,4 +1,11 @@
-nombre = prompt(`Nombre: `)
-horas = Number(prompt(`Cantidad de horas: `))
-total = horas*15300
-alert(`Hola ${nombre}, el total de pago por sus horas de trabajo es ${total}`)
+examen1 = Number(prompt(`Nota del examen 1: `))
+examen2 = Number(prompt(`Nota del examen 2: `))
+examen3 = Number(prompt(`Nota del examen 3: `))
+examen4 = Number(prompt(`Nota del examen 4: `))
+nota = (examen1+examen2+examen3+examen4)/4
+if (nota<3){
+    alert(`No aprobo`)
+}else{
+    alert(`Aprobo`)
+}
+alert(`promedio de las calificacione es ${nota}`)
