5e0848d5e1b748b1d9bdc4cbbf64b5b235dea335
JoseLuisReySaavedraADSO <reysaavedra123@gmail.com>
2022-11-08 07:41:18 -0500

    Punto 12

--- main.js
+++ main.js
@@ -1,6 +1,21 @@
-alert(`Sabemos que para calcular el volumen de un elipsoide\nV = (4/3) * PI * alto * ancho * alto`)
-let a = Number(prompt(`altura del elipsoide: `))
-let an = Number(prompt(`anchura del elipsoide: `))
-let l = Number(prompt(`largo del elipsoide: `))
-vol = (4/3)*(Math.PI)*a*an*l
-console.log(`Entonces el volumen del elipsoide es ${vol}`)
+let duracion = prompt(`¿Quiere expresar la duracion en horas o minutos?`)
+let valor = 355
+if(duracion == "minutos"){
+    minut = Number(prompt(`Cuantos minutos duro la llamada? `))
+    cost = minut*355
+    aumento = cost*(20/100)
+    valorca = cost+aumento
+    iva1 = valorca-cost
+    console.log(`El valor de su llamada sin iva es ${cost} Pesos\nEl costo de la llamada con el iva es ${valorca} Pesos\nUsted pago ${iva1} Pesos de iva`)
+}else{if (duracion == "horas"){
+        hrs = Number(prompt(`Cuantos horas duro la llamada? `))
+    minut = hrs*60
+    cost = minut*355
+    aumento = cost*(20/100)
+    valorca = cost+aumento
+    iva1 = valorca-cost
+    console.log(`El valor de su llamada sin iva es ${cost} Pesos\nEl costo de la llamada con el iva es ${valorca} Pesos\nUsted pago ${iva1} Pesos de iva`)
+    }else{
+        alert(`Ingresa una opcion valida!!`)
+    }
+}
