428c2914a6ae8777b209aef7141ccd28cc97a320
JoseLuisReySaavedraADSO <reysaavedra123@gmail.com>
2022-11-08 07:30:17 -0500

    Punto 11

--- main.js
+++ main.js
@@ -1,13 +1,6 @@
-alert(`Vamos a obtener las raices reales de\nA * x2 +b * x + c\nsabiendo que x es un valor constante igual a 15`)
-let a = Number(prompt(`Ingresar el valor de a`))
-let b = Number(prompt(`Ingresar el valor de b`))
-let c = Number(prompt(`Ingresar el valor de c`))
-let x = 15
-d = (b*b)-4*a*c
-if (d<0){
-    alert(`No existen soluciones reales!!`)
-}else{
-    d1=(-b+Math.sqrt(d))/(2*a)
-    d2=(-b-Math.sqrt(d))/(2*a)
-    console.log(`_____________SOLUCION_____________\nSolucion x1: ${d1}\nSolucion x2: ${d2}`)
-}
+alert(`Sabemos que para calcular el volumen de un elipsoide\nV = (4/3) * PI * alto * ancho * alto`)
+let a = Number(prompt(`altura del elipsoide: `))
+let an = Number(prompt(`anchura del elipsoide: `))
+let l = Number(prompt(`largo del elipsoide: `))
+vol = (4/3)*(Math.PI)*a*an*l
+console.log(`Entonces el volumen del elipsoide es ${vol}`)
