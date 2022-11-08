d00e62bd352455ca59ecdb3f75ec4f45903fee89
JoseLuisReySaavedraADSO <reysaavedra123@gmail.com>
2022-11-08 06:56:46 -0500

    Punto 5

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
-alert(`Calcularemos el area de un triangulo...`)
-let num1 = Number(prompt(`Cual es la base de su triangulo? `))
-let num2 = Number(prompt(`Cual es la altura de su triangulo? `))
-area = (num1*num2)/2
-console.log(`Segun eso tenemos que el resultado seria = ${area}`)
+alert(`Calcularemos el area de una circunferencia...`)
+let num1 = Number(prompt(`Tenemos que el radio de nuestro radio es:`))
+area = (Math.PI)*Math.pow(num1, 2)
+long = 2*(Math.PI)*num1
+console.log(`El area de la circunferencia es ${area}`)
+console.log(`La longitud de la circunferencia es ${long}`)
