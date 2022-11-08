6cc6823b89cfe0fb93f108348dbb03df21327365
JoseLuisReySaavedraADSO <reysaavedra123@gmail.com>
2022-11-08 07:00:05 -0500

    Punto 6

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
-alert(`Calcularemos el area de una circunferencia...`)
-let num1 = Number(prompt(`Tenemos que el radio de nuestro radio es:`))
-area = (Math.PI)*Math.pow(num1, 2)
-long = 2*(Math.PI)*num1
-console.log(`El area de la circunferencia es ${area}`)
-console.log(`La longitud de la circunferencia es ${long}`)
+alert(`Vamos a calcular la velocidad de un proyecti...`)
+let num1 = Number(prompt(`Cual es la distancia que recorre en kilometros el proyectil? `))
+let num2 = Number(prompt(`En cuantos minutos los recorre? `))
+vel = (num1*1000)/(num2*60)
+console.log(`Teniendo esto claro tenemos que nuestro resultado en m/s seria= ${vel}`)
